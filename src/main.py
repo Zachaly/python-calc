6bd50a525c700f94f4a8ea4a83da286478814adf
Zachariasz Soja <zachary3003@gmail.com>
2022-06-27 21:47:41 +0200

    Calculations are now possible

--- src/main.py
+++ src/main.py
@@ -6,18 +6,22 @@ class App(tk.Tk):
         super().__init__()
 
         self.resizable(0, 0)
-
+        self.calcSign = ""
         self.defaultFont = Font(family="Arial", size=24)
         self.fakeImg = tk.PhotoImage(width=1, height=1)
 
         self.addColums()
         self.addRows()
 
+        self.previusNumber = 0
+        self.previusNumberLabel = tk.Label(compound="r", font=Font(family="Arial", size=16), text=str(self.previusNumber))
+        self.previusNumberLabel.grid(row=0, column=0, columnspan=4, sticky=tk.E)
+
         self.numberLabel = tk.Label(compound="r", font=self.defaultFont)
-        self.numberLabel.grid(row=0, column=0, columnspan=4, sticky=tk.E)
+        self.numberLabel.grid(row=1, column=0, columnspan=4, sticky=tk.E)
 
         currentnumber = 1
-        currentrow = 1
+        currentrow = 2
         currentcolumn = 0
         while currentnumber <= 9:
             if(currentnumber == 4 or currentnumber == 7):
@@ -27,28 +31,75 @@ class App(tk.Tk):
             currentnumber += 1
             currentcolumn += 1
         
-        self.numberButton(0, 4, 0)
+        self.numberButton(0, 5, 0)
+
+        self.defaultButton(".", self.addDot, 5, 1)
+        self.defaultButton("del", self.deleteNum, 5, 2)
+        self.defaultButton("+", lambda: self.setCalcSign("+"), 2, 3)
+        self.defaultButton("-", lambda: self.setCalcSign("-"), 3, 3)
+        self.defaultButton("/", lambda: self.setCalcSign("/"), 4, 3)
+        self.defaultButton("*", lambda: self.setCalcSign("*"), 5, 3)
+        self.defaultButton("=", self.equals, 6, 0)
 
     def numberButton(self, number, row, column):
-        button = tk.Button(self, text=str(number),
+        return self.defaultButton(str(number), lambda: self.addNumber(str(number)), row, column)
+
+    def defaultButton(self, text, command, row, column):
+        button = tk.Button(self, text=text,
         image=self.fakeImg,
         font=self.defaultFont,
         width=100,
         height=100,
         compound="c",
-        command=lambda: self.addNumber(number)) 
+        command=command)
+
         button.grid(row=row, column=column)
 
+        return button
+
     def addNumber(self, number):
-        self.numberLabel["text"] += str(number)
+        self.numberLabel["text"] += number
 
     def addColums(self):
         for i in range(4):
             self.columnconfigure(i, weight=1)
 
     def addRows(self):
-        for i in range(4):
+        for i in range(6):
             self.rowconfigure(i, weight=1)
+
+    def addDot(self):
+        if "." in self.numberLabel["text"]:
+            return
+        if len(self.numberLabel["text"]) == 0 :
+            self.numberLabel["text"] += "0"
+        self.numberLabel["text"] += "."
+    
+    def deleteNum(self):
+        if len(self.numberLabel["text"]) == 0:
+            return
+        
+        self.numberLabel["text"] = self.numberLabel["text"][:-1]
+
+    def setCalcSign(self, sign):
+        self.previusNumber = float(self.numberLabel["text"])
+        self.previusNumberLabel["text"] = str(self.previusNumber)
+        self.numberLabel["text"] = ""
+        self.calcSign = sign
+
+    def equals(self):
+        number = float(self.numberLabel["text"])
+
+        if self.calcSign == "+":
+            self.numberLabel["text"] = str(self.previusNumber + number)
+        elif self.calcSign == "-":
+            self.numberLabel["text"] = str(self.previusNumber - number)
+        elif self.calcSign == "*":
+            self.numberLabel["text"] = str(self.previusNumber * number)
+        elif self.calcSign == "/":
+            self.numberLabel["text"] = str(self.previusNumber / number)
+        
+        self.previusNumberLabel["text"] = str(number)
 
 app = App()
 app.mainloop()
